f2163edfde32b19d1f1c44e5db7d3d17490e0dbc
gkpty <gkardonski@gmail.com>
2020-05-04 00:13:18 -0500

    added dropzone

--- js/dz.js
+++ js/dz.js
@@ -1,21 +1,14 @@
-// ************************ Drag and drop ***************** //
-let dropArea = document.getElementById("drop-area")
-
-// Prevent default drag behaviors
-;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+let dropArea = document.getElementById("drop-area");
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
   dropArea.addEventListener(eventName, preventDefaults, false)   
   document.body.addEventListener(eventName, preventDefaults, false)
-})
-
-// Highlight drop area when item is dragged over it
-;['dragenter', 'dragover'].forEach(eventName => {
+});
+['dragenter', 'dragover'].forEach(eventName => {
   dropArea.addEventListener(eventName, highlight, false)
-})
-
-;['dragleave', 'drop'].forEach(eventName => {
+});
+['dragleave', 'drop'].forEach(eventName => {
   dropArea.addEventListener(eventName, unhighlight, false)
-})
-
+});
 // Handle dropped files
 dropArea.addEventListener('drop', handleDrop, false)
 
@@ -33,9 +26,19 @@ function unhighlight(e) {
 }
 
 function handleDrop(e) {
-  var dt = e.dataTransfer
-  var files = dt.files
-  handleFiles(files)
+  var ext = e.dataTransfer.files[0].name.split('.')[1]
+  console.log(ext)
+  if(ext === 'html' || ext === 'md' || ext === 'txt'){
+    document.getElementById('fileName').textContent = e.dataTransfer.files[0].name
+    document.getElementById('progress-bar').hidden = false
+    var dt = e.dataTransfer
+    var files = dt.files
+    handleFiles(files)
+  }
+  else{
+    document.getElementById('progress-bar').hidden = true
+    document.getElementById('fileName').textContent = 'Error. only html, md and txt files supported.'
+  }
 }
 
 let uploadProgress = []
@@ -61,41 +64,124 @@ function handleFiles(files) {
   files = [...files]
   initializeProgress(files.length)
   files.forEach(uploadFile)
-  files.forEach(previewFile)
-}
-
-function previewFile(file) {
-  let reader = new FileReader()
-  reader.readAsDataURL(file)
-  reader.onloadend = function() {
-    let img = document.createElement('img')
-    img.src = reader.result
-    document.getElementById('gallery').appendChild(img)
-  }
 }
 
 function uploadFile(file, i) {
-  var url = 'https://api.cloudinary.com/v1_1/joezimim007/image/upload'
-  var xhr = new XMLHttpRequest()
-  var formData = new FormData()
-  xhr.open('POST', url, true)
-  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
-
-  // Update progress (can be used to show progress indicator)
-  xhr.upload.addEventListener("progress", function(e) {
+  var num = 0
+  var ext = ""
+  const reader = new FileReader();
+  if (file){
+    reader.readAsText(file);
+    num = document.getElementById('adjuntos_qty')
+    ext = file.name.split('.').pop().toLowerCase()
+  }
+  reader.addEventListener("progress", function(e) {
     updateProgress(i, (e.loaded * 100.0 / e.total) || 100)
   })
-
-  xhr.addEventListener('readystatechange', function(e) {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      updateProgress(i, 100) // <- Add this
+  reader.addEventListener('loadend', function(e) {
+    updateProgress(i, 100) // <- Add this
+  }) 
+  reader.addEventListener("load", function () {
+    //convert image file to base64 string
+    console.log(reader.result.length)
+    //AWS API gateway hast a limit of 10MB.
+    if(reader.result.length < 10380000){
+      var sizeMb = reader.result.length*(1/1000000)
+      console.log('total upload size: ', sizeMb)
+      document.getElementById('fileBody').value = reader.result;
+      console.log(reader.result)
     }
-    else if (xhr.readyState == 4 && xhr.status != 200) {
-      // Error. Inform the user
-    }
-  })
-
-  formData.append('upload_preset', 'ujpu6gyk')
-  formData.append('file', file)
-  xhr.send(formData)
+    else {alert('Ha excedido el Limite de 10MB de adjuntos.')};
+  }, false);
 }
+
+function removePreview(elem){
+  var colId = elem+'col'
+  var preview = document.getElementById(colId)
+  var item = document.getElementById(elem)
+  var itemSize = 0
+  if(item.src) itemSize = item.src.length
+  else if(item.data) itemSize = item.data.length
+  preview.remove()
+  document.getElementById('adjuntos_qty').value -= 1
+  document.getElementById('attchSize').value = parseInt(document.getElementById('attchSize').value) - itemSize
+}
+
+
+function removePreview(elem){
+var colId = elem+'col'
+var preview = document.getElementById(colId)
+var item = document.getElementById(elem)
+var itemSize = 0
+if(item.src) itemSize = item.src.length
+else if(item.data) itemSize = item.data.length
+preview.remove()
+document.getElementById('adjuntos_qty').value -= 1
+document.getElementById('attchSize').value = parseInt(document.getElementById('attchSize').value) - itemSize
+}
+
+$(function(){
+$('#consultaspanama-form').submit(function(e){
+  console.log('Submitting ...')
+  e.preventDefault();
+  let captcha = grecaptcha.getResponse();
+  if(captcha.length < 1){
+    alert('please fill out the recaptcha')
+  }
+  else {
+    //get attachments size
+    var attachments = ""
+    var delim = ''
+    var siz = $('#adjuntos_qty').val();
+    if(siz > 0){
+      for(var i =0; i < siz; i++){
+        if(i>0) delim = '*,*'
+        id = '#attch'+i;		
+        if($(id)){
+          if($(id).is('img')){
+            if($(id).attr("src") && $(id).attr("src").length > 0) attachments = attachments + delim + $(id).attr("src")
+            else console.log('image upload error: no src attribute present')
+          }
+          else if($(id).is('div')){
+            if($(id).attr("data") && $(id).attr("data").length > 0) attachments = attachments + delim + $(id).attr("data")
+            else console.log('image upload error: no src attribute present')
+          }
+        }
+      }
+    }
+    console.log(attachments.length)
+    // ** add the number of attachments to the fields
+    console.log({ "id": "","nombre": $('#nombre').val(),"correo": $('#correo').val(),"telefono": $('#telefono').val(),"tipo": $('#tipo').val(),"consulta": $('#consulta').val(),"adjuntos": attachments,"adjuntos_qty":$('#adjuntos_qty').val(), "captcha":captcha })
+    $.ajax({
+      type: "POST",
+      url: "https://1p7rjld7p9.execute-api.us-east-1.amazonaws.com/DeploymentStage/",
+      dataType: "json",
+      contentType: "application/json",
+      data: JSON.stringify( { "id": "","nombre": $('#nombre').val(),"correo": $('#correo').val(),"telefono": $('#telefono').val(),"tipo": $('#tipo').val(),"consulta": $('#consulta').val(),"adjuntos": attachments,"adjuntos_qty":$('#adjuntos_qty').val(), "captcha":captcha } ),
+      beforeSend: function(data) {
+          $('#consultaspanama-btn').prop('disabled', true);
+          $('#consultaspanama-form :input').prop('disabled', true);
+          $('#consultaspanama-status').html('Enviando...').show();
+      },
+      success: function(data, status, jqXHR) {
+        console.log(data);
+        if(data === 'Success'){
+          $('#consultaspanama-status').text("Su consulta ha sido enviada Exitosamente.").show();
+          $('#consultaspanama-form :input').removeProp('disabled');
+          $('#consultaspanama-btn').removeProp('disabled');
+        }
+        else {
+          $('#consultaspanama-status').text('Ha habido un inconveniente... Porfavor intente denuevo').show();
+          $('#consultaspanama-form :input').removeProp('disabled');
+          $('#consultaspanama-btn').removeProp('disabled');
+        }
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        $('#consultaspanama-status').text('Error. Porfavor revise su coneccion al internet e intente denuevo').show();
+        $('#consultaspanama-form :input').removeProp('disabled');
+        $('#consultaspanama-btn').removeProp('disabled');
+      }
+    });
+  }
+}); 				
+});
