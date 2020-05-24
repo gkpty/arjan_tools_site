d58fc81af128cedc573c85bd55305fac33bcd475
gkpty <gkardonski@gmail.com>
2020-05-24 03:41:37 -0500

    added index, audit pade drafts. added the animation to arjans head on hover.

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function nodArjan(mod){
+  let name = "arjan-logo-"+mod;
+  document.getElementById(name).classList.add("nodArjan")
+}
+
+function removeNod(mod){
+  let name = "arjan-logo-"+mod;
+  document.getElementById(name).classList.remove("nodArjan")
+}
