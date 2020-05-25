56a6efa6091998879fbd87b75d944fb85532a1ca
gkpty <gkardonski@gmail.com>
2020-05-25 01:13:19 -0500

    added nav animation

--- js/index.js
+++ js/index.js
@@ -7,3 +7,15 @@ function removeNod(mod){
   let name = "arjan-logo-"+mod;
   document.getElementById(name).classList.remove("nodArjan")
 }
+
+window.onload = function() {
+  setTimeout(() => loadnav(), 2000)
+};
+
+function loadnav(){
+  var loadwords = document.getElementsByClassName('load-word')
+  for(var lw of loadwords) {
+    lw.classList.remove('d-none')
+    lw.classList.add('fade-in')
+  }
+}
